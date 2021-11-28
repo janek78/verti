59942b30394183f21d12057e4a4d5ffdd5562ed3
Janusz T <janek78@yahoo.com>
2021-11-28 11:32:23 +0100

    gulpfile modified

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,20 @@
 // Ścieżka do aktualnie wykonywanego zadania
 
 const entryPath = "src/styles";
-
-
 const gulp = require("gulp");
-
 const sass = require("gulp-sass")(require("sass"));
-
 const sourcemaps = require("gulp-sourcemaps");
-
 const autoprefixer = require("gulp-autoprefixer");
-
 const browserSync = require("browser-sync").create();
-
 
 function compileSass(done) {
 
     gulp
-
         .src(entryPath + "/styles.scss")
-
         .pipe(sourcemaps.init())
-
         .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
-
         .pipe(autoprefixer())
-
         .pipe(sourcemaps.write("."))
-
         .pipe(gulp.dest("./public/css"));
 
 
